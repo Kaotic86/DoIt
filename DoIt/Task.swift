3126731ac7ef2b689a6585c23dd1929c1da33569
Sam Gonzalez <Cammo5@msn.com>
2017-01-12 12:39:09 -0500

    added classes

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Sam Gonzalez on 1/12/17.
+//  Copyright © 2017 Gonzalez Apps. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
